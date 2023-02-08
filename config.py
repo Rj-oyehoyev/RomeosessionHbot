5bfff182c4a6ac862c27f3dbfbad62f15c9540c9
RJ-5 <124252004+RJ-5@users.noreply.github.com>
2023-02-08 18:37:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "")) 
 API_HASH = getenv("API_HASH", "") 
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+TOKEN = getenv("TOKEN", "")
 START_PIC = getenv("START_PIC", "")
